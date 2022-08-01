c68aef4d1bda0860cf6cae42376c3ee389074e3d
Deshan Nawanjana <deshan.uok@gmail.com>
2022-08-01 22:26:38 +0530

    Create SkyBox

--- index.js
+++ index.js
@@ -1,16 +1,93 @@
-const load = () => {
-    const img = document.querySelector('body > img')
-    const cli = img.getBoundingClientRect()
-    img.style.left = (window.innerWidth - cli.width) / 2 + 'px'
-    img.style.top = (window.innerHeight - cli.height) / 2 + 'px'
-    img.addEventListener('click', () => location = 'https://github.com/deshan-nawanjana/')
-    setTimeout(() => img.style.opacity = 1, 100)
+// current angle
+let ax = 0
+// mouse down flag
+let md = false
+// auto rotate direction
+let dr = 1
+// root element
+const root = document.querySelector('#root')
+
+// circle coords method
+const circle = (radius, angle) => {
+    // convert to radian
+    const theta = (angle - 90) * Math.PI / 180
+    // return x y coords in circle
+    return {
+        a : Math.cos(theta) * radius,
+        b : Math.sin(theta) * radius * - 1
+    }
 }
 
-window.addEventListener('load', load)
-window.addEventListener('resize', load)
+// get children array of root
+const children = Array.from(root.children)
 
-window.addEventListener('contextmenu', event => {
-    event.preventDefault()
-    return false
+// update method
+const update = () => {
+    // for each child
+    for(let i = 0; i < children.length; i++) {
+        // current child
+        const child = children[i]
+        // get angle for child
+        const cax = ax + 45 * -i
+        // get child coords in circle
+        const cxz = circle(620, cax)
+        // set active prop by position
+        child.setAttribute('active', cxz.b > 200 ? 1 : 0)
+        // update transform styles
+        child.style.transform = `
+            translateX(calc(calc(50vw - 50%) - ${cxz.a}px))
+            translateY(calc(calc(50vh - 50%) - 0px))
+            translateZ(calc(${cxz.b}px - 450px))
+            rotateY(${-cax}deg)
+        `
+    }
+}
+
+// rotate method
+const rotate = () => {
+    // auto rotate only not hold
+    if(md === false) { ax += 0.1 * dr }
+    // update children
+    update()
+    // request next frame
+    requestAnimationFrame(rotate)
+}
+
+// mousedown listener
+window.addEventListener('mousedown', () => {
+    // update cursor
+    root.style.cursor = 'grabbing'
+    // update mousedown flag
+    md = true
 })
+
+// mouseup listener
+window.addEventListener('mouseup', () => {
+    // update cursor
+    root.style.cursor = 'grab'
+    // update mousedown flag
+    md = false
+})
+
+// mousemove listener
+window.addEventListener('mousemove', event => {
+    // only if mouse down
+    if(md === true) {
+        // update angle
+        ax -= event.movementX * 0.1
+        // clear overflow
+        ax = ax % 360
+        // update direction on drag right
+        if(event.movementX > 0) { dr = -1 }
+        // update direction on drag left
+        if(event.movementX < 0) { dr = +1 }
+    }
+})
+
+// profile link onlick
+document.querySelector('.profile-link').addEventListener('click', () => {
+    window.open('https://github.com/deshan-nawanjana')
+})
+
+// start rotate loop onload
+window.addEventListener('load', rotate)

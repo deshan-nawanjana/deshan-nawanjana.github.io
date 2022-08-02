acf43192de61bee95a8978c0e9250cef6255610d
Deshan Nawanjana <deshan.uok@gmail.com>
2022-08-02 10:07:28 +0530

    API Loader for GitHub Profile Info

---
+++ assets/scripts/api_loader.js
@@ -0,0 +1,15 @@
+const requestAPI = (url, body = null) => {
+    return new Promise((resolve, reject) => {
+        const endpoint = 'https://api.github.com/' + url
+        const options = {}
+        if(body !== null) { options.body = JSON.stringify(body) }
+        fetch(endpoint, options).then(x => x.json()).then(resolve).catch(reject)
+    })
+}
+
+const loadAPIs = async function() {
+    const data = {}
+    data.profile = await requestAPI('users/deshan-nawanjana')
+    data.repos = await requestAPI('users/deshan-nawanjana/repos')
+    return data
+}

4bfaf514c76a5020c4c5645fb7cbc178798ba163
Deshan Nawanjana <deshan.uok@gmail.com>
2022-08-04 00:08:05 +0530

    APIs Fetch by Dev Mode

--- assets/scripts/api_loader.js
+++ assets/scripts/api_loader.js
@@ -1,16 +1,26 @@
+// method to request from endpoints
 const requestAPI = (url, prg) => {
+    // return promise
     return new Promise((resolve, reject) => {
+        // get endpoint full path
         const endpoint = url.indexOf(':') > -1 || url[0] === '.'
             ? url : 'https://api.github.com/' + url
+        // fetch options
         const options = { method : 'GET' }
+        // fetch request
         fetch(endpoint, options).then(x => x.json()).then(data => {
+            // calculate progress
             prg = prg / 100 * 400
+            // update progress bar
             qs('.loading-bar').style.boxShadow = `inset ${prg}px 0px 0px 0px #FFF8`
+            // resolve data
             resolve(data)
+            // reject when error
         }).catch(reject)
     })
 }
 
+// method to get cached data
 const cacheCheck = () => {
     // get cache data from local storage
     const cache = localStorage['api-data']
@@ -32,6 +42,7 @@ const cacheCheck = () => {
     }
 }
 
+// method to load all apis
 const loadAPIs = async function() {
     // get cached data
     const cache = cacheCheck()
@@ -44,10 +55,18 @@ const loadAPIs = async function() {
         qs('.loading-bar').style.display = 'block'
         // data object
         const data = {}
-        // request from each endpoints
-        data.profile = await requestAPI('users/deshan-nawanjana', 20)
-        data.repos = await requestAPI('users/deshan-nawanjana/repos?per_page=100', 60)
-        data.calendar = await requestAPI('https://apis.dnjs.info/github_contributions/', 100)
+        // check dev mode
+        if(dev) {
+            // fetch from each sample data
+            data.profile = await requestAPI('./local/data_profile.json', 20)
+            data.repos = await requestAPI('./local/data_repos.json', 60)
+            data.calendar = await requestAPI('./local/data_calendar.json', 100)
+        } else {
+            // request from each endpoints
+            data.profile = await requestAPI('users/deshan-nawanjana', 20)
+            data.repos = await requestAPI('users/deshan-nawanjana/repos?per_page=100', 60)
+            data.calendar = await requestAPI('https://apis.dnjs.info/github_contributions/', 100)
+        }
         // get calendar nested data
         data.calendar = data.calendar.data.user.contributionsCollection.contributionCalendar
         // sort repo array by starts

73c3fe59fc3eb41ae4befbe5c0af48a37e2b99b1
Deshan Nawanjana <deshan.uok@gmail.com>
2022-08-03 15:19:06 +0530

    Cache Handler

--- assets/scripts/api_loader.js
+++ assets/scripts/api_loader.js
@@ -11,17 +11,41 @@ const requestAPI = (url, prg) => {
     })
 }
 
+const cacheCheck = () => {
+    const cache = localStorage['api-data']
+    if(cache) {
+        const data = JSON.parse(cache)
+        if(Date.now() - data.time < 30 * 60 * 60 * 1000) {
+            return data.data
+        } else {
+            return null
+        }
+    } else {
+        return null
+    }
+}
+
 const loadAPIs = async function() {
-    // data object
-    const data = {}
-    // request from each endpoints
-    data.profile = await requestAPI('users/deshan-nawanjana', 20)
-    data.repos = await requestAPI('users/deshan-nawanjana/repos?per_page=100', 60)
-    data.calendar = await requestAPI('https://apis.dnjs.info/github_contributions/', 100)
-    // get calendar nested data
-    data.calendar = data.calendar.data.user.contributionsCollection.contributionCalendar
-    // sort repo array by starts
-    data.repos = data.repos.sort((a, b) => a.stargazers_count > b.stargazers_count ? -1 : 1)
-    // return data
-    return data
+    // get cached data
+    const cache = cacheCheck()
+    // check cache availability
+    if(cache) {
+        // return cache
+        return cache
+    } else {
+        // data object
+        const data = {}
+        // request from each endpoints
+        data.profile = await requestAPI('users/deshan-nawanjana', 20)
+        data.repos = await requestAPI('users/deshan-nawanjana/repos?per_page=100', 60)
+        data.calendar = await requestAPI('https://apis.dnjs.info/github_contributions/', 100)
+        // get calendar nested data
+        data.calendar = data.calendar.data.user.contributionsCollection.contributionCalendar
+        // sort repo array by starts
+        data.repos = data.repos.sort((a, b) => a.stargazers_count > b.stargazers_count ? -1 : 1)
+        // store on cache
+        localStorage['api-data'] = JSON.stringify({ data : data, time : Date.now() })
+        // return data
+        return data
+    }
 }

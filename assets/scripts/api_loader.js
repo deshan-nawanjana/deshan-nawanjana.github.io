27b89442c27ee7139410425a0b49e3d0aea316a2
Deshan Nawanjana <deshan.uok@gmail.com>
2024-12-23 13:34:37 +0530

    Update Indent Levels

--- assets/scripts/api_loader.js
+++ assets/scripts/api_loader.js
@@ -1,79 +1,79 @@
 // method to request from endpoints
 const requestAPI = (url, prg) => {
-    // return promise
-    return new Promise((resolve, reject) => {
-        // get endpoint full path
-        const endpoint = url.indexOf(':') > -1 || url[0] === '.'
-            ? url : 'https://api.github.com/' + url
-        // fetch options
-        const options = { method : 'GET' }
-        // fetch request
-        fetch(endpoint, options).then(x => x.json()).then(data => {
-            // calculate progress
-            prg = prg / 100 * 400
-            // update progress bar
-            qs('.loading-bar').style.boxShadow = `inset ${prg}px 0px 0px 0px #FFF8`
-            // resolve data
-            resolve(data)
-            // reject when error
-        }).catch(reject)
-    })
+  // return promise
+  return new Promise((resolve, reject) => {
+    // get endpoint full path
+    const endpoint = url.indexOf(':') > -1 || url[0] === '.'
+      ? url : 'https://api.github.com/' + url
+    // fetch options
+    const options = { method: 'GET' }
+    // fetch request
+    fetch(endpoint, options).then(x => x.json()).then(data => {
+      // calculate progress
+      prg = prg / 100 * 400
+      // update progress bar
+      qs('.loading-bar').style.boxShadow = `inset ${prg}px 0px 0px 0px #FFF8`
+      // resolve data
+      resolve(data)
+      // reject when error
+    }).catch(reject)
+  })
 }
 
 // method to get cached data
 const cacheCheck = () => {
-    // get cache data from local storage
-    const cache = localStorage['api-data']
-    // if cache available
-    if(cache) {
-        // parse cache data
-        const data = JSON.parse(cache)
-        // check cache expire time for 30 mins
-        if(Date.now() - data.time < 30 * 60 * 1000) {
-            // return valid cache
-            return data.data
-        } else {
-            // cache expired
-            return null
-        }
+  // get cache data from local storage
+  const cache = localStorage['api-data']
+  // if cache available
+  if (cache) {
+    // parse cache data
+    const data = JSON.parse(cache)
+    // check cache expire time for 30 mins
+    if (Date.now() - data.time < 30 * 60 * 1000) {
+      // return valid cache
+      return data.data
     } else {
-        // no cache
-        return null
+      // cache expired
+      return null
     }
+  } else {
+    // no cache
+    return null
+  }
 }
 
 // method to load all apis
-const loadAPIs = async function() {
-    // get cached data
-    const cache = cacheCheck()
-    // check cache availability
-    if(cache) {
-        // return cache
-        return cache
+const loadAPIs = async function () {
+  // get cached data
+  const cache = cacheCheck()
+  // check cache availability
+  if (cache) {
+    // return cache
+    return cache
+  } else {
+    // display progress bar
+    qs('.loading-bar').style.display = 'block'
+    // data object
+    const data = {}
+    // check dev mode
+    if (dev) {
+      // fetch from each sample data
+      data.profile = await requestAPI('./local/data_profile.json', 20)
+      data.repos = await requestAPI('./local/data_repos.json', 60)
+      data.calendar = await requestAPI('./local/data_calendar.json', 100)
     } else {
-        // display progress bar
-        qs('.loading-bar').style.display = 'block'
-        // data object
-        const data = {}
-        // check dev mode
-        if(dev) {
-            // fetch from each sample data
-            data.profile = await requestAPI('./local/data_profile.json', 20)
-            data.repos = await requestAPI('./local/data_repos.json', 60)
-            data.calendar = await requestAPI('./local/data_calendar.json', 100)
-        } else {
-            // request from each endpoints
-            data.profile = await requestAPI('users/deshan-nawanjana', 20)
-            data.repos = await requestAPI('users/deshan-nawanjana/repos?per_page=100', 60)
-            data.calendar = await requestAPI('https://apis.dnjs.info/github/contributions.php', 100)
-        }
-        // get calendar nested data
-        data.calendar = data.calendar.data.user.contributionsCollection.contributionCalendar
-        // sort repo array by starts
-        data.repos = data.repos.sort((a, b) => a.stargazers_count > b.stargazers_count ? -1 : 1)
-        // store on cache
-        localStorage['api-data'] = JSON.stringify({ data : data, time : Date.now() })
-        // return data
-        return data
+      // request from each endpoints
+      data.profile = await requestAPI('users/deshan-nawanjana', 20)
+      data.repos = await requestAPI('users/deshan-nawanjana/repos?per_page=100', 60)
+      data.calendar = await requestAPI('https://apis.dnjs.lk/github/contributions.php', 100)
     }
+    // get calendar nested data
+    data.calendar = data.calendar.data.user.contributionsCollection.contributionCalendar
+    // sort repo array by starts
+    data.repos = data.repos.sort((a, b) => a.stargazers_count > b.stargazers_count ? -1 : 1)
+    // store on cache
+    localStorage['api-data'] = JSON.stringify({ data: data, time: Date.now() })
+    // return data
+    return data
+  }
 }
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let qs = x => document.querySelector(x)
 
 // dev mode flag
-const dev = location.toString().includes('localhost')
+const dev = false
 // current angle
 let ax = 0
 // mouse down flag
@@ -22,13 +22,13 @@ const load = qs('.loading-screen')
 
 // circle coords method
 const circle = (radius, angle) => {
-    // convert to radian
-    const theta = (angle - 90) * Math.PI / 180
-    // return x y coords in circle
-    return {
-        a : Math.cos(theta) * radius,
-        b : Math.sin(theta) * radius * - 1
-    }
+  // convert to radian
+  const theta = (angle - 90) * Math.PI / 180
+  // return x y coords in circle
+  return {
+    a: Math.cos(theta) * radius,
+    b: Math.sin(theta) * radius * - 1
+  }
 }
 
 // get children array of root
@@ -36,36 +36,36 @@ const children = Array.from(root.children)
 
 // update method
 const update = () => {
-    // toggle z gap by window orientation
-    gz = window.innerWidth < window.innerHeight && window.innerWidth < 1080 ? 3200 : 450
-    // for each child
-    for(let i = 0; i < children.length; i++) {
-        // current child
-        const child = children[i]
-        // get angle for child
-        const cax = ax + 45 * -i
-        // get child coords in circle
-        const cxz = circle(620, cax)
-        // set active prop by position
-        child.setAttribute('active', cxz.b - 450 > -185.038 ? 1 : 0)
-        // update transform styles
-        child.style.transform = `
-            translateX(calc(calc(50vw - 50%) - ${cxz.a}px))
-            translateY(calc(calc(50vh - 50%) - 0px))
-            translateZ(calc(${cxz.b}px - ${gz}px))
-            rotateY(${-cax}deg)
-        `
-    }
+  // toggle z gap by window orientation
+  gz = window.innerWidth < window.innerHeight && window.innerWidth < 1080 ? 3200 : 450
+  // for each child
+  for (let i = 0; i < children.length; i++) {
+    // current child
+    const child = children[i]
+    // get angle for child
+    const cax = ax + 45 * -i
+    // get child coords in circle
+    const cxz = circle(620, cax)
+    // set active prop by position
+    child.setAttribute('active', cxz.b - 450 > -185.038 ? 1 : 0)
+    // update transform styles
+    child.style.transform = `
+      translateX(calc(calc(50vw - 50%) - ${cxz.a}px))
+      translateY(calc(calc(50vh - 50%) - 0px))
+      translateZ(calc(${cxz.b}px - ${gz}px))
+      rotateY(${-cax}deg)
+    `
+  }
 }
 
 // rotate method
 const rotate = () => {
-    // auto rotate only not hold
-    if(md === false && hv === false && rt) { ax += 0.06 * dr }
-    // update children
-    update()
-    // request next frame
-    requestAnimationFrame(rotate)
+  // auto rotate only not hold
+  if (md === false && hv === false && rt) { ax += 0.06 * dr }
+  // update children
+  update()
+  // request next frame
+  requestAnimationFrame(rotate)
 }
 
 // rotate ignore elements
@@ -73,52 +73,52 @@ const ignore = ['loading-screen']
 
 // mousedown listener
 window.addEventListener('mousedown', event => {
-    // return while loading screen showing
-    if(ignore.some(x => event.target.classList.contains(x))) { return }
-    // update cursor
-    root.style.cursor = 'grabbing'
-    // update mousedown flag
-    md = true
+  // return while loading screen showing
+  if (ignore.some(x => event.target.classList.contains(x))) { return }
+  // update cursor
+  root.style.cursor = 'grabbing'
+  // update mousedown flag
+  md = true
 })
 
 // mouseup listener
 window.addEventListener('mouseup', event => {
-    // return while loading screen showing
-    if(ignore.some(x => event.target.classList.contains(x))) { return }
-    // update cursor
-    root.style.cursor = 'grab'
-    // update mousedown flag
-    md = false
+  // return while loading screen showing
+  if (ignore.some(x => event.target.classList.contains(x))) { return }
+  // update cursor
+  root.style.cursor = 'grab'
+  // update mousedown flag
+  md = false
 })
 
 // mousemove listener
 window.addEventListener('mousemove', event => {
-    // return while loading screen showing
-    if(ignore.some(x => event.target.classList.contains(x))) { return }
-    // only if mouse down
-    if(md === true) {
-        // update angle
-        ax -= event.movementX * 0.1
-        // clear overflow
-        ax = ax % 360
-        // update direction on drag right
-        if(event.movementX > 0) { dr = -1 }
-        // update direction on drag left
-        if(event.movementX < 0) { dr = +1 }
+  // return while loading screen showing
+  if (ignore.some(x => event.target.classList.contains(x))) { return }
+  // only if mouse down
+  if (md === true) {
+    // update angle
+    ax -= event.movementX * 0.1
+    // clear overflow
+    ax = ax % 360
+    // update direction on drag right
+    if (event.movementX > 0) { dr = -1 }
+    // update direction on drag left
+    if (event.movementX < 0) { dr = +1 }
+  }
+  // only if mouse not down
+  if (md === false) {
+    // check for hovering any active card
+    if (event.composedPath().some(x => {
+      return x.getAttribute && x.hasAttribute('active')
+    })) {
+      // disable auto rotate from hover flag
+      hv = true
+    } else {
+      // enable auto rotate from hover flag
+      hv = false
     }
-    // only if mouse not down
-    if(md === false) {
-        // check for hovering any active card
-        if(event.composedPath().some(x => {
-            return x.getAttribute && x.hasAttribute('active')
-        })) {
-            // disable auto rotate from hover flag
-            hv = true
-        } else {
-            // enable auto rotate from hover flag
-            hv = false
-        }
-    }
+  }
 })
 
 // method to calculate total of an array
@@ -126,38 +126,38 @@ const total = arr => arr.reduce((x, y) => x + y)
 
 // method to start
 const request = () => {
-    // laod data from apis
-    loadAPIs().then(data => {
-        // call each generator
-        Object.values(cards).forEach(x => x(data))
-        // get delay time for timout
-        const delay = dev ? 0 : 300
-        // timeout
-        setTimeout(() => {
-            // decrease loading screen opacity
-            load.style.opacity = 0
-            // start rotate
-            rotate()
-            // timeout
-            setTimeout(() => {
-                // hide loading screen
-                load.style.display = 'none'
-            }, delay)
-        }, delay)
-    })
+  // load data from apis
+  loadAPIs().then(data => {
+    // call each generator
+    Object.values(cards).forEach(x => x(data))
+    // get delay time for timeout
+    const delay = dev ? 0 : 300
+    // timeout
+    setTimeout(() => {
+      // decrease loading screen opacity
+      load.style.opacity = 0
+      // start rotate
+      rotate()
+      // timeout
+      setTimeout(() => {
+        // hide loading screen
+        load.style.display = 'none'
+      }, delay)
+    }, delay)
+  })
 }
 
 // click event on window
 window.addEventListener('click', event => {
-    // check for open window attribute
-    if(event.target.hasAttribute('click-open')) {
-        // get link to open
-        let link = event.target.getAttribute('click-open')
-        // replace from github profile url
-        link = link.replace('{PROFILE}', 'https://github.com/deshan-nawanjana')
-        // open in new window
-        window.open(link)
-    }
+  // check for open window attribute
+  if (event.target.hasAttribute('click-open')) {
+    // get link to open
+    let link = event.target.getAttribute('click-open')
+    // replace from github profile url
+    link = link.replace('{PROFILE}', 'https://github.com/deshan-nawanjana')
+    // open in new window
+    window.open(link)
+  }
 })
 
 // request api data onload
